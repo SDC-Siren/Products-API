6bf9bbd82de8ebe85867349e5405426e1b6aea92
Loren <loren.johnson99@yahoo.com>
2023-07-12 11:08:38 -0700

    completes postgres model

--- database/model.sql
+++ database/model.sql
@@ -7,3 +7,50 @@ CREATE TABLE IF NOT EXISTS product {
   default_price INT,
   PRIMARY KEY (id)
 }
+
+CREATE TABLE IF NOT EXISTS features {
+  id INT NOT NULL AUTO_INCREMENT,
+  product_id INT NOT NULL,
+  feature VARCHAR(30) NOT NULL,
+  value VARCHAR(25),
+  PRIMARY KEY (id),
+  FOREIGN KEY (product_id) REFERENCES product(id)
+}
+
+CREATE TABLE IF NOT EXISTS styles {
+  id INT NOT NULL AUTO_INCREMENT,
+  product_id INT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  sale_price INT,
+  original_price INT,
+  default_style BOOL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (product_id) REFERENCES product(id)
+}
+
+CREATE TABLE IF NOT EXISTS photos {
+  id INT NOT NULL AUTO_INCREMENT,
+  style_id INT NOT NULL,
+  url VARCHAR(135),
+  thumbnail_url VARCHAR(135),
+  PRIMARY KEY (id),
+  FOREIGN KEY (style_id) REFERENCES styles(id)
+}
+
+CREATE TABLE IF NOT EXISTS skus {
+  id INT NOT NULL AUTO_INCREMENT,
+  style_id INT NOT NULL,
+  size VARCHAR(3),
+  quantity INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (style_id) REFERENCES styles(id)
+}
+
+CREATE TABLE IF NOT EXISTS related {
+  id INT NOT NULL AUTO_INCREMENT,
+  current_product_id INT NOT NULL,
+  related_product_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (current_product_id) REFERENCES product(id),
+  FOREIGN KEY (related_product_id) REFERENCES product(id)
+}
