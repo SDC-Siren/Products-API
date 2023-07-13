2dae12d6878b837869cc37d2a0f547dc59ca42d3
Loren <loren.johnson99@yahoo.com>
2023-07-13 15:09:42 -0700

    changes to client method

--- database/index.js
+++ database/index.js
@@ -1,45 +1,43 @@
-const { Pool } = require('pg');
+const { Client } = require('pg');
 const fs = require('fs');
 const path = require('path');
 
-const pool = new Pool({
-  user: 'lorenjohnson',
-  database: 'atelierProducts'
-});
+const client = new Client();
+async function setUpDatabase() {
+  await client.connect();
+  const schema = fs.readFileSync(path.join(__dirname, '/schema.sql')).toString();
+  const etl = fs.readFileSync(path.join(__dirname, '/etl.sql')).toString();
 
-// running the SQL file executes its statements and create schema
-const schema = fs.readFileSync(path.join(__dirname, '/schema.sql')).toString();
-const etl = fs.readFileSync(path.join(__dirname, '/etl.sql')).toString();
+  client.query(schema, (err) => {
+    if (err) {
+      console.log('Error executing SQL file', err);
+    }
+  })
 
+  client.query(etl, (err) => {
+    if (err) {
+      console.log('Error executing SQL file', err);
+    }
+  });
 
-pool.query(schema, (err) => {
-  if (err) {
-    console.log('Error executing SQL file', err);
-  }
-})
+  client.query(`SELECT * FROM product`, (err, res) => {
+    if (err) {
+      console.log('Error executing query', err);
+    } else {
+      console.log(res);
+    }
+    client.end();
+  });
 
-pool.query(etl, (err) => {
-  if (err) {
-    console.log('Error executing SQL file', err);
-  }
-});
-
-pool.query(`SELECT * FROM product`, (err, res) => {
-  if (err) {
-    console.log('Error executing query', err);
-  } else {
-    console.log(res);
-  }
-  pool.end();
-});
-
-// query tables created from schema
-// pool.query(`SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE';`, (err, res) => {
-//   if (err) {
-//     console.error('Error executing query', err);
-//   } else {
-//     console.log(res);
-//   }
-//   // End the database connection
-//   pool.end();
-// });
+  // query tables created from schema
+  // client.query(`SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE';`, (err, res) => {
+  //   if (err) {
+  //     console.error('Error executing query', err);
+  //   } else {
+  //     console.log(res);
+  //   }
+  //   // End the database connection
+  //   client.end();
+  // });
+}
+setUpDatabase();
