467794281675455a34ec19ad643a47e7b3dd27bc
Loren <loren.johnson99@yahoo.com>
2023-07-13 20:05:27 -0700

    saves progress

--- database/etl.sql
+++ database/etl.sql
@@ -10,11 +10,17 @@ COPY styles
   FROM '/Users/lorenjohnson/Desktop/HackReacter/SDC/CSV/cleanStyles.csv'
   CSV HEADER;
 
-COPY photos
-  FROM '/Users/lorenjohnson/Desktop/HackReacter/SDC/CSV/photos.csv'
-  CSV HEADER;
--- CLEANUP : VARCHAR TOO LONG FOR LINK
+-- CLEANUP : VARCHAR TOO LONG THUMBNAIL URL
+-- COPY photos
+--   FROM '/Users/lorenjohnson/Desktop/HackReacter/SDC/CSV/photos.csv'
+--   CSV HEADER;
 
+-- CLEANUP : SOME STYLE IDS DO NOT EXIST IN STYLE TABLE
 -- COPY skus
 --   FROM '/Users/lorenjohnson/Desktop/HackReacter/SDC/CSV/skus.csv'
+--   CSV HEADER;
+
+-- CLEANUP: 'Key (related_product_id)=(0) is not present in table "product".
+-- COPY related
+--   FROM '/Users/lorenjohnson/Desktop/HackReacter/SDC/CSV/related.csv'
 --   CSV HEADER;
