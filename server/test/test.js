2d092d454d11977fc99249ef86c73359c1ba342b
Loren <loren.johnson99@yahoo.com>
2023-07-15 15:46:20 -0700

    finally got tests working

--- server/test/test.js
+++ server/test/test.js
@@ -1,25 +1,25 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const request = require('supertest');
-// const app = require('../index.js');
 
 chai.use(chaiHttp);
 let expect = chai.expect;
+console.log(global.Promise);
 
 describe('GET products', () => {
-  it('should send first 5 products', () => {
-    chai.request('http://localhost:3000')
+  it('should send first 5 products', (done) => {
+    return chai.request('http://localhost:3000')
       .get('/products')
       .end((error, response) => {
         expect(error).to.be.null;
         expect(response).to.have.status(200);
         expect(response.body).to.be.a('array');
         expect(response.body).to.have.lengthOf(5);
+        done();
       });
   });
 
-  it('should send 3 products from page 2', () => {
-    chai.request('http://localhost:3000')
+  it('should send 3 products from page 2', (done) => {
+    return chai.request('http://localhost:3000')
       .get('/products')
       .query({page: 2, count: 3})
       .end((error, response) => {
@@ -28,11 +28,12 @@ describe('GET products', () => {
         expect(response.body).to.be.a('array');
         expect(response.body).to.have.lengthOf(3);
         expect(response.body[0].id).to.equal(4);
+        done();
       });
   });
 
-  it('should send empty array if requested page outside of database', () => {
-    chai.request('http://localhost:3000')
+  it('should send empty array if requested page outside of database', (done) => {
+    return chai.request('http://localhost:3000')
       .get('/products')
       .query({page: 2342, count: 25432})
       .end((error, response) => {
@@ -40,25 +41,27 @@ describe('GET products', () => {
         expect(response).to.have.status(200);
         expect(response.body).to.be.a('array');
         expect(response.body).to.have.lengthOf(0);
+        done();
       });
   });
 
 });
 
 describe('GET product details', () => {
-  it('should return single product with features', () => {
+  it('should return single product with features', (done) => {
     let product_id = 905434;
-    chai.request('http://localhost:3000')
+    return chai.request('http://localhost:3000')
       .get(`/products/${905434}`)
       .end((error, response) => {
         expect(error).to.be.null;
         expect(response).to.have.status(200);
         expect(response.body).to.be.an('object');
         expect(response.body.id).to.equal(product_id);
+        done();
       });
   });
 
-  it('should respond if product does not exist in database', () => {
+  it('should respond if product does not exist in database', (done) => {
     let product_id = 403514878;
     chai.request('http://localhost:3000')
       .get(`/products/${403514878}`)
@@ -67,7 +70,8 @@ describe('GET product details', () => {
         expect(response).to.have.status(404);
         expect(response.body).to.be.a('string');
         expect(response.body.message).to.equal('This product does not exist.');
-      })
+        done();
+      });
   });
 
 });
