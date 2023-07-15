b25dc1c057cbbbd99b972cd08b22c667756fc339
Loren <loren.johnson99@yahoo.com>
2023-07-14 17:21:20 -0700

    get product query retrieves product info from db, still needs to get right page and count

--- database/models.js
+++ database/models.js
@@ -6,8 +6,16 @@ const pool = new Pool({
 });
 
 
-module.exports.getProducts = function() {
+module.exports.getProducts = async function(page = 1, count = 5) {
   console.log('getting products');
+
+  // get ids for page 1, 5 per page
+  let ids = [32, 4534]
+  const response = await pool.query({
+    text: 'SELECT name, slogan, description, category, default_price FROM product WHERE id = ANY($1)',
+    values: [ids]
+  });
+  return response.rows;
 };
 
 module.exports.getInfo = function(product_id) {
@@ -20,4 +28,4 @@ module.exports.getStyles = function(product_id) {
 
 module.exports.getRelated = function(product_id) {
   console.log('getting related products');
-}
+};
--- server/routes.js
+++ server/routes.js
@@ -3,22 +3,30 @@ const router = express.Router();
 const { getProducts, getInfo, getStyles, getRelated } = require('../database/models.js')
 
 router.get('/products', (req, res) => {
-  getProducts();
-  res.status(200).send('you want products?');
+  const page = req.query.page;
+  const count = req.query.count;
+
+  getProducts(page, count)
+    .then((response) => {
+      res.status(200).send(response);
+    }).catch((error) => {
+      console.log(error);
+      res.sendStatus(500);
+    });
 });
 
 router.get('/products/:product_id/', (req, res) => {
-  getInfo();
+  getInfo(req.params.product_id);
   res.status(200).send(`${req.params.product_id}`);
 });
 
 router.get('/products/:product_id/styles', (req, res) => {
-  getStyles();
+  getStyles(req.params.product_id);
   res.status(200).send(`${req.params.product_id} styles`);
 });
 
 router.get('/products/:product_id/related', (req, res) => {
-  getRelated();
+  getRelated(req.params.product_id);
   res.status(200).send(`${req.params.product_id} related`);
 });
 
